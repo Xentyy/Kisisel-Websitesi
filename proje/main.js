fdd6f7d4a8994e36866b63fa6bd5a67eb6e970fe
Melih <127318460+Xentyy@users.noreply.github.com>
2023-05-21 00:06:03 +0300

    Add files via upload

--- proje/main.js
+++ proje/main.js
@@ -1,10 +1,35 @@
-function getData(url)
-{
-    fetch(url)
-    .then((response)=> response.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err))
-    
-}
+const url = 'https://online-movie-database.p.rapidapi.com/auto-complete?q=game';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '73a1c64b95mshed39c679117b5bcp181168jsnae5186a05b3f',
+		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+	}
+};
 
-getData("https://jsonplaceholder.typicode.com/users");
+fetch(url, options)
+	.then(response => response.json())
+	.then(data => {
+		const list = data.d;
+
+		list.forEach(item => {
+			const name = item.l;
+			const poster = item.i.imageUrl;
+			const movie = `<li><img src="${poster}"><h2>${name}</h2></li>`;
+			document.querySelector('.movies').innerHTML += movie;
+		});
+	})
+	.catch(error => console.error(error));
+
+
+
+
+
+
+
+
+
+
+
+
+
